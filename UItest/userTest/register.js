84ccf03409630548b5687691c6db5a7060d13512
wenqi0927 <1272379959@qq.com>
2017-10-16 23:05:28 +0800

    修改注册代码

--- UItest/userTest/register.js
+++ UItest/userTest/register.js
@@ -17,7 +17,7 @@ let { Builder } = require('selenium-webdriver')
 let registerPage = require('../../config/uiconfig/registerPage')
 let loginPage = require('../../config/uiconfig/loginPage')
 let indexPage = require('../../config/uiconfig/indexPage')
-
+let data = require('../../testdata/register.json')
 let app = require('../../config/app.confifg')
 let uiAction = require('../../common/ui/uiaction')
 let dbAction = require('../../common/ui/db')
@@ -61,7 +61,7 @@ describe('注册功能', function () {
         it('两次密码不一致 应该收到<两次密码输入不一致。>提示', async function () {
             await driver.get(app.baseUrl);
             let nowdate = new Date().valueOf();
-            await uiAction.userRegister(driver,nowdate,"123456","345612",nowdate+"@163.com")
+            await uiAction.userRegister(driver,nowdate,data.repaerrData.password,data.repaerrData.repassword,nowdate+"@163.com")
             let errortip = await driver.findElement(registerPage.errortip).getText();
             //验证提示信息是否正确
             return assert.ok(errortip.indexOf("两次密码输入不一致。") > 0);
@@ -69,14 +69,14 @@ describe('注册功能', function () {
         it('email 格式不正确,提示 邮箱不合法。',async function(){
             await driver.get(app.baseUrl);
             let nowdate = new Date().valueOf();
-            await uiAction.userRegister(driver,nowdate,"123456","123456",nowdate+"163.com")
+            await uiAction.userRegister(driver,nowdate,data.emailerrData.password,data.emailerrData.repassword,nowdate+"163.com")
             let errortip = await driver.findElement(registerPage.errortip).getText();
             //邮箱不合法。
             return assert.ok(errortip.indexOf("邮箱不合法。") > -1);
         })
         it('用户名存在，提示用户名或邮箱已被使用。',async function(){
             await driver.get(app.baseUrl);
-            await uiAction.userRegister(driver,"1505399200170","123456","123456","1272379959@qq.com")
+            await uiAction.userRegister(driver,data.existData.username,data.existData.password,data.existData.repassword,data.existData.email)
             let errortip = await driver.findElement(registerPage.errortip).getText();
             //用户名或邮箱已被使用。
             return assert.ok(errortip.indexOf("用户名或邮箱已被使用。"));
@@ -85,7 +85,7 @@ describe('注册功能', function () {
     it('注册成功后是否显示激活提示信息', async function () {
         await driver.get(app.baseUrl);
         let nowdate = new Date().valueOf();
-        await uiAction.userRegister(driver,nowdate,"123456","123456",nowdate+"@163.com")
+        await uiAction.userRegister(driver,nowdate,data.existData.password,data.existData.repassword,nowdate+"@163.com")
         let successtip = await driver.findElement(registerPage.successtip).getText();
         //欢迎加入 Nodeclub！我们已给您的注册邮箱发送了一封邮件，请点击里面的链接来激活您的帐号。
         let actualtip = "欢迎加入 Nodeclub！我们已给您的注册邮箱发送了一封邮件，请点击里面的链接来激活您的帐号。"
@@ -97,7 +97,7 @@ describe('注册功能', function () {
     it('使用未激活的账户登录', async function () {
         await driver.get(app.baseUrl);
         let nowdate = new Date().valueOf();
-        await uiAction.userRegister(driver,nowdate,"123456","123456",nowdate+"@qq.com")
+        await uiAction.userRegister(driver,nowdate,data.existData.password,data.existData.repassword,nowdate+"@qq.com")
         await uiAction.userLogin(driver,nowdate,"123456");
         //此帐号还没有被激活，激活链接已发送到 1506689011794@163.com 邮箱，请查收。
         let actualtip = `此帐号还没有被激活，激活链接已发送到 '${nowdate}'@qq.com 邮箱，请查收。`
