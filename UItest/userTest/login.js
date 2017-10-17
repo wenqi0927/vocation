c5a79ed2a3298dcf4d9a7c18ae628debcd394d2b
wenqi0927 <1272379959@qq.com>
2017-10-17 22:29:07 +0800

    优化登录

--- UItest/userTest/login.js
+++ UItest/userTest/login.js
@@ -1,3 +1,16 @@
+/**
+ * 
+ * 1. 登录窗口是否可以正常打开？
+ * 2. 登录窗口UI显示正常
+ * 3. 用户名及密码为空
+ * 4. 用户名为空
+ * 5. 密码为空
+ * 6. 密码错误
+ * 7. 用户名错误
+ * 8. 用户名及密码均错误
+ * 9. 密码正确，登陆成功
+ */
+
 let uiAction = require('../../common/ui/uiaction')
 let loginPage = require('../../config/uiconfig/loginPage')
 let app = require('../../config/app.confifg')
@@ -6,6 +19,7 @@ require ("chromedriver")
 var assert = require('assert');
 var webdriver = require('selenium-webdriver');
 var driver = new webdriver.Builder().forBrowser('chrome').build();
+let data = require("../../testdata/login.json")
 
 describe('用户登录',function(){
 
@@ -26,34 +40,52 @@ describe('用户登录',function(){
         // 验证页面中的元素是否存在
         return assert.ok(driver.findElement({ css: "body > div.navbar > div > div > ul > li:nth-child(6) > a" }));
     })
+    it('登录窗口UI显示正常？', async function () {
+        await driver.get(app.baseUrl);
+        await driver.findElement(indexpage.loginhref).click();
+        // 验证页面中的元素是否存在
+        return assert.ok(driver.findElement({ css: "body > div.navbar > div > div > ul > li:nth-child(6) > a" }));
+    })
     describe('登陆信息验证',function(){
         it('用户名及密码为空',async function(){
             await driver.get(app.baseUrl); 
-            await uiAction.userLogin(driver,'','')
+            await uiAction.userLogin(driver,data.emptyerr.username,data.emptyerr.password)
             let errortip = await driver.findElement(loginPage.errortip).getText();
             return assert.ok(errortip.indexOf("信息不完整。"));      
         })
         it('用户名为空',async function(){
             await driver.get(app.baseUrl); 
-            await uiAction.userLogin(driver,'','123456')
+            await uiAction.userLogin(driver,data.emptyerr.username,data.user.password)
             let errortip = await driver.findElement(loginPage.errortip).getText();
             return assert.ok(errortip.indexOf("信息不完整。"));      
         })
         it('密码为空',async function(){
             await driver.get(app.baseUrl); 
-            await uiAction.userLogin(driver,'1505399200170','')
+            await uiAction.userLogin(driver,data.user.username,data.emptyerr.password)
             let errortip = await driver.findElement(loginPage.errortip).getText();
             return assert.ok(errortip.indexOf("信息不完整。"));      
         })
         it('密码错误',async function(){
             await driver.get(app.baseUrl); 
-            await uiAction.userLogin(driver,'1505399200170','654321')
+            await uiAction.userLogin(driver,data.user.username,data.err.password)
+            let errortip = await driver.findElement(loginPage.errortip).getText();
+            return assert.deepEqual('×\n用户名或密码错误',errortip);      
+        })
+        it('用户名错误',async function(){
+            await driver.get(app.baseUrl); 
+            await uiAction.userLogin(driver,data.err.username,data.user.password)
+            let errortip = await driver.findElement(loginPage.errortip).getText();
+            return assert.deepEqual('×\n用户名或密码错误',errortip);      
+        })
+        it('用户名及密码均错误',async function(){
+            await driver.get(app.baseUrl); 
+            await uiAction.userLogin(driver,data.err1.username,data.err1.password)
             let errortip = await driver.findElement(loginPage.errortip).getText();
             return assert.deepEqual('×\n用户名或密码错误',errortip);      
         })
         it('密码正确，登陆成功',async function(){
             await driver.get(app.baseUrl);
-            await uiAction.userLogin(driver,'1505399200170','123456')
+            await uiAction.userLogin(driver,data.user.username,data.user.password)
             return assert.ok(driver.findElement({css:'body > div.navbar > div > div > ul > li:nth-child(7) > a'}))
 
         })
